7df5653589c0d205d99f46ebd8ab220b64c998d3
Jorge Olvera Cuenca <jorge.olvera@jarsa.com.mx>
2021-12-14 12:57:15 -0600

    [FIX]report_xlsx:validation was added for docids task#9121

--- report_xlsx/controllers/main.py
+++ report_xlsx/controllers/main.py
@@ -36,6 +36,8 @@ class ReportController(report.ReportController):
                 if data["context"].get("lang"):
                     del data["context"]["lang"]
                 context.update(data["context"])
+                if not docids and data["context"].get("active_ids"):
+                    docids = data["context"].get("active_ids")
             xlsx = report.with_context(context).render_xlsx(docids, data=data)[0]
             report_name = report.report_file
             if report.print_report_name and not len(docids) > 1:
